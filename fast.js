d77b12f487810157fd5d92e636046653a11851d4
Wajidur Rahman <wrfuad0@gmail.com>
2021-01-19 17:53:43 +0600

    add two file

---
+++ fast.js
@@ -0,0 +1,3 @@
+var numl = "99.5";
+var num2 = 15;
+console.log(num1+num2);
---
+++ hello.js
@@ -0,0 +1,36 @@
+var bananaPrise = 12;
+var seenAfter = 21;
+console.log(typeof seenAfter);
+
+
+var nameM = "mark zukerbag";
+console.log(typeof nameM);
+
+var isHot = true;
+var isRich = false;
+console.log(typeof isHot);
+
+// var name = string likkar nimo tin ta `` '' ""
+var promise = "I promis i will work hard to become a programer";
+console.log(promise.split(' '));
+
+
+// integer 2 float  
+// var number1 = 25;
+// var number2 = 15.5;
+// number2 = parseFloat(number2);
+// console.log(number1+number2);
+
+var number1 = 0.1;
+var number2 = 0.2;
+// var total = number1 + number2;
+// total = total.toFixed(2);
+// console.log(total);
+
+
+// +zug -big  /Vag %vagshes(modiulas) *gun
+
+var price1 = 25;
+var prise2 = 35;
+var total = price1 + prise2;
+console.log(total);
